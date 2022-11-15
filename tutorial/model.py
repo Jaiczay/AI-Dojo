8adefdf5e49d9f19e119e03aa0f3523c5e59bbae
jaizo <manuel.jaiczay@gmail.com>
2022-11-15 13:31:20 +0100

    added CNN model

--- tutorial/model.py
+++ tutorial/model.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Any
 import torch.nn as nn
 
 
@@ -21,3 +22,46 @@ class MyNN(nn.Module):
         x = self.sigmoid(x)
         x = self.output_layer(x)
         return x
+
+
+class MyCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.input_layer = BasicConv2d(1, 8, kernel_size=3)
+        self.hidden_layer_1 = BasicConv2d(8, 16, kernel_size=3)
+        self.hidden_layer_2 = BasicConv2d(16, 16, kernel_size=3, stride=2)
+        self.hidden_layer_3 = BasicConv2d(16, 16, kernel_size=3, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.output_layer = nn.Linear(16, 10)
+
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = self.hidden_layer_1(x)
+        x = self.hidden_layer_2(x)
+        x = self.hidden_layer_3(x)
+        # print(x.shape)
+        x = torch.flatten(self.avgpool(x), 1)
+        x = self.output_layer(x)
+        return x
+
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, **kwargs: Any) -> None:
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+
+if __name__ == "__main__":
+    model = MyNN()
+    print("MyNN amount of parameters: ", sum(p.numel() for p in model.parameters()))
+    model = MyCNN()
+    print("MyCNN amount of parameters: ", sum(p.numel() for p in model.parameters()))
--- tutorial/train.py
+++ tutorial/train.py
@@ -2,14 +2,14 @@ import os
 
 import torch
 from torch.utils.data import DataLoader
-from torchvision import transforms
+from torchvision.transforms import Compose, ToTensor, RandomRotation
 from torchvision.datasets import MNIST
 
 from metrics import Metrics
-from model import MyNN
+from model import MyNN, MyCNN
 
 
-def start_training(device_name="cpu", lr=0.1, epochs=10, batch_size=5, num_workers=2, lr_gamma=0.6, data_path="./data"):
+def start_training(device_name="cuda:0", lr=0.1, epochs=15, batch_size=16, num_workers=4, lr_gamma=0.7, data_path="./data"):
     # setting the device (CPU or GPU)
     device = torch.device(device_name)
 
@@ -17,10 +17,11 @@ def start_training(device_name="cpu", lr=0.1, epochs=10, batch_size=5, num_worke
     if not os.path.exists(data_path):
         os.mkdir(data_path)
 
-    train_set = MNIST(data_path, train=True, download=True, transform=transforms.ToTensor())
+    # train_set = MNIST(data_path, train=True, download=True, transform=Compose([RandomRotation(10), ToTensor()]))
+    train_set = MNIST(data_path, train=True, download=True, transform=ToTensor())
     train_dataloader = DataLoader(train_set, batch_size=batch_size, num_workers=num_workers, shuffle=True)
 
-    valid_set = MNIST(data_path, train=False, download=True, transform=transforms.ToTensor())
+    valid_set = MNIST(data_path, train=False, download=True, transform=ToTensor())
     valid_dataloader = DataLoader(valid_set, batch_size=batch_size, num_workers=num_workers, shuffle=True)
 
     # Initializing the model, cost/loss function and the optimizer (stochastic gradient descent)
@@ -36,6 +37,7 @@ def start_training(device_name="cpu", lr=0.1, epochs=10, batch_size=5, num_worke
         print('#' * 40, end=" ")
         print('Epoch {}/{}'.format(epoch+1, epochs), end=" ")
         print('#' * 40)
+        print("Current learning rate: ", lr_scheduler.get_last_lr()[0])
 
         for phase in ["training", "validating"]:
 
